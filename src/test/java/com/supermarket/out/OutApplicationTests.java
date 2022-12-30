99482d4095856c5670eddba3b9383c48f082286f
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 03:34:58 +0530

    Add Project

--- src/test/java/com/supermarket/out/OutApplicationTests.java
+++ src/test/java/com/supermarket/out/OutApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OutApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
