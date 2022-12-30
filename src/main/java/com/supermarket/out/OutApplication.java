6c3a65acff306ca25c2ec1a127c860a11a17cab8
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2022-12-31 03:17:03 +0530

    first commit

---
+++ src/main/java/com/supermarket/out/OutApplication.java
@@ -0,0 +1,13 @@
+package com.supermarket.out;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supermarket/out/OutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supermarket.out;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
