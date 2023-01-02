9b2d07b9bf989de1ad32c97d255c0cec5ff5b29c
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-02 11:07:10 +0530

    Add services pakage and class

---
+++ src/main/java/com/supermarket/out/service/UserService.java
@@ -0,0 +1,29 @@
+package com.supermarket.out.service;
+
+import com.supermarket.out.dto.UserDTO;
+import com.supermarket.out.entity.User;
+import com.supermarket.out.repo.UserRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UserDTO saveUser(UserDTO userDTO){
+        userRepo.save(modelMapper.map(userDTO, User.class));
+        return userDTO;
+    }
+}
