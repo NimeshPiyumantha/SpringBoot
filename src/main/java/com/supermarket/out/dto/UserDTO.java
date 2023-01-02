d32375ea75bb1c2cc7f0aa02491d1171b59e3b04
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-02 11:06:32 +0530

    Add dto pakage and class

---
+++ src/main/java/com/supermarket/out/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.supermarket.out.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserDTO {
+        private int id;
+        private String name;
+        private String address;
+
+}
