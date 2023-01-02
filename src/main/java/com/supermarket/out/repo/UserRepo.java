6b33e2786af543aef59e636a34bb373aa29355d9
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-02 15:11:51 +0530

    Refactor Content

--- src/main/java/com/supermarket/out/repo/UserRepo.java
+++ src/main/java/com/supermarket/out/repo/UserRepo.java
@@ -2,6 +2,7 @@ package com.supermarket.out.repo;
 
 import com.supermarket.out.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 /**
@@ -12,6 +13,7 @@ public interface UserRepo extends JpaRepository<User, Integer> {
     @Query(value = "SELECT * FROM USER WHERE ID = ?1", nativeQuery = true)
     User getUserById(String userId);
 
+    //    @Modifying kiyna eka use krnwa update query liyddi
     @Query(value = "SELECT * FROM USER WHERE ID = ?1 AND ADDRESS =?2", nativeQuery = true)
     User getUserByUserIdAndAddress(String userId, String address);
 
