70112d1cc12a223ea0a64e9ef774f899912512da
Nimesh_Piyumanth <88945963+NimeshPiyumantha@users.noreply.github.com>
2023-01-04 20:22:36 +0530

    Refactor Content

--- src/main/java/com/supermarket/out/entity/User.java
+++ src/main/java/com/supermarket/out/entity/User.java
@@ -1,10 +1,11 @@
 package com.supermarket.out.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * @author : Nimesh Piyumantha
--- src/main/java/com/supermarket/out/repo/UserRepo.java
+++ src/main/java/com/supermarket/out/repo/UserRepo.java
@@ -2,7 +2,6 @@ package com.supermarket.out.repo;
 
 import com.supermarket.out.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 /**
